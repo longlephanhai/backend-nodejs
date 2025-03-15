7ece016330638055034bd7f0ac340d20872d09bd
LePhanHaiLong <longlph.23it@gmail.com>
2025-03-16 00:43:36 +0700

    mysql with nodejs

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 const configViewEngine = require('./config/viewEngine')
 const router = require('./routes/web')
-
+// Get the client
+const mysql = require('mysql2');
 const port = process.env.PORT || 8080
 const hostName = process.env.HOST_NAME
 
@@ -12,6 +13,25 @@ configViewEngine(app)
 
 // khai báo route
 app.use(router)
+
+// test connection
+// Create the connection to database
+const connection = mysql.createConnection({
+  port: 3306, 
+  host: 'localhost',
+  user: 'root',
+  password: 'log?1234',
+  database: 'longlephanhai',
+});
+
+// A simple SELECT query
+connection.query(
+  'SELECT * FROM users',
+  function (err, results, fields) {
+    console.log("results: ", results); // results contains rows returned by server
+    console.log("fields: ", fields); // fields contains extra meta data about results, if available
+  }
+);
 
 app.listen(port, hostName, () => {
   console.log(`Example app listening on port ${port}`)
