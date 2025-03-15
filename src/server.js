58b32b2f851bf858cb66ffe5784a7d2a8b10cba3
LePhanHaiLong <longlph.23it@gmail.com>
2025-03-15 22:48:37 +0700

    static files

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
 require('dotenv').config()
-
 const port = process.env.PORT || 8080
 const hostName = process.env.HOST_NAME
 // config template engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+// config static file
+app.use(express.static(path.join(__dirname, 'public')))
 
-
+// khai báo route
 app.get('/', (req, res) => {
   res.send('Hello World with me!')
 })
