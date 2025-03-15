b0863843a7f2e4bb8dd3a3a0f21aa03b20125db1
LePhanHaiLong <longlph.23it@gmail.com>
2025-03-15 22:26:44 +0700

    env

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 8080
 
+require('dotenv').config()
+
+const port = process.env.PORT || 8080
+const hostName = process.env.HOST_NAME
 // config template engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -16,6 +19,6 @@ app.get('/abc', (req, res) => {
   res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port, hostName, () => {
   console.log(`Example app listening on port ${port}`)
 })
